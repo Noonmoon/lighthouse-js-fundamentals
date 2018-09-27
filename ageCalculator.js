f967be050bb552967ef945e93a8ae27647e0aeb7
vagrant <Tristan Deering>
2018-09-27 14:14:01 +0000

    for and while problem

--- ageCalculator.js
+++ ageCalculator.js
@@ -2,4 +2,4 @@ function ageCalculator(name, yearOfBirth, currentYear) {
   var age = currentYear - yearOfBirth;
   return name + " is " + age + " years old.";
 }
-ageCalculator("Miranda", 1983, 2015));
+ageCalculator("Miranda", 1983, 2015);
--- okay.js
+++
@@ -1,2 +0,0 @@
-
-console.log("Jeremy" > "Khurram")
